93b4ea7fde4a3fa07dc3bf4ba9dcf9e57ef92fda
Sven Konings <svenkonings@live.nl>
2020-09-02 16:35:58 +0200

    Escape class names in NumberOfChildren regex and filter out null trees

--- CodeAnalysis/src/main/scala/codeAnalysis/metrics/baseline/NumberOfChildren.scala
+++ CodeAnalysis/src/main/scala/codeAnalysis/metrics/baseline/NumberOfChildren.scala
@@ -3,6 +3,8 @@ package codeAnalysis.metrics.baseline
 import codeAnalysis.analyser.Compiler
 import codeAnalysis.analyser.metric.{Metric, MetricProducer, MetricResult, ObjectMetric}
 
+import scala.util.matching.Regex
+
 object NumberOfChildren extends MetricProducer {
   override def apply(compiler: Compiler): Metric = new NumberOfChildren(compiler)
 }
@@ -14,14 +16,12 @@ class NumberOfChildren(override val compiler: Compiler) extends ObjectMetric {
   def numberOfChildren(tree: global.ImplDef): Int = {
     val name = tree.symbol.nameString
     val qualifiedName = tree.symbol.qualifiedName
-    val nameFilter = s"(extends|with)\\s+$name".r
+    val nameFilter = s"(extends|with)\\s+${Regex.quote(name)}".r
     compiler.loadedSources
       .filter(source => nameFilter.findFirstIn(new String(source.content)).isDefined)
-      .map(source => compiler
-        .treeFromLoadedSource(source)
-        .asInstanceOf[global.Tree]
-        .count { case tree: global.ImplDef => tree.symbol.parentSymbols.exists(_.qualifiedName == qualifiedName) }
-      )
+      .map(source => compiler.treeFromLoadedSource(source).asInstanceOf[global.Tree])
+      .filter(_ != null)
+      .map(_.count { case tree: global.ImplDef => tree.symbol.parentSymbols.exists(_.qualifiedName == qualifiedName) })
       .sum
   }
 
